e3af2ceafc95a7c96a23746555a002a417892dc0
ykmdaifuku <t236090@st.yamagata-u.ac.jp>
2024-11-15 18:41:08 +0900

    Add files via upload

---
+++ 1115kadai1.c
@@ -0,0 +1,19 @@
+int main()
+{
+    int a, b, c;
+    
+    a = 594;
+    b = -34;
+    c = 57;
+    
+    if ( a > 0)
+    printf("aは正の数です\n");
+    if (b < 0)
+    printf("bは負の数です\n");
+    if (a > c)
+    printf("aはcより大きいです\n");
+    if(c % 2 != 0)
+    printf("cは奇数です\n");
+
+    return 0;
+}
